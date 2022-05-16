54c28174e68c7455572c9c624ee9264071696bce
cpot5620 <wjswjdfhr030@gmail.com>
2022-05-16 10:56:25 +0900

    chat service beta

---
+++ ChattingApp/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ChattingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.txt_all = new System.Windows.Forms.TextBox();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.txt_msg = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_all
+            // 
+            this.txt_all.Location = new System.Drawing.Point(1, 1);
+            this.txt_all.Multiline = true;
+            this.txt_all.Name = "txt_all";
+            this.txt_all.Size = new System.Drawing.Size(668, 366);
+            this.txt_all.TabIndex = 0;
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(688, 111);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(100, 46);
+            this.btn_Connect.TabIndex = 1;
+            this.btn_Connect.Text = "접속";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(683, 379);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(105, 46);
+            this.btn_Send.TabIndex = 2;
+            this.btn_Send.Text = "전송";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // txt_msg
+            // 
+            this.txt_msg.Location = new System.Drawing.Point(1, 392);
+            this.txt_msg.Name = "txt_msg";
+            this.txt_msg.Size = new System.Drawing.Size(668, 25);
+            this.txt_msg.TabIndex = 3;
+            this.txt_msg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_msg_KeyDown);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(688, 37);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(100, 50);
+            this.btnCreate.TabIndex = 4;
+            this.btnCreate.Text = "방 만들기";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txt_msg);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.txt_all);
+            this.Name = "Form1";
+            this.Text = "Chatting";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox txt_all;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.TextBox txt_msg;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
