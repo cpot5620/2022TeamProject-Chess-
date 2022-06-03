78dd690129028086b8acb3e80e2b24ffcb1c652d
hongjae95 <kjhonggg95@gmail.com>
2022-06-03 23:34:46 +0900

    22.06.03 add skin shop

---
+++ DBtest/Form5.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace DBtest
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.skinPic5 = new System.Windows.Forms.PictureBox();
+            this.skinPic6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.skinPic1 = new System.Windows.Forms.PictureBox();
+            this.skinPic2 = new System.Windows.Forms.PictureBox();
+            this.skinPic3 = new System.Windows.Forms.PictureBox();
+            this.skinPic4 = new System.Windows.Forms.PictureBox();
+            this.lb_name2 = new System.Windows.Forms.Label();
+            this.lb_pts2 = new System.Windows.Forms.Label();
+            this.lb_pts = new System.Windows.Forms.Label();
+            this.lb_name = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.lb_name2);
+            this.panel1.Controls.Add(this.lb_pts2);
+            this.panel1.Controls.Add(this.lb_pts);
+            this.panel1.Controls.Add(this.lb_name);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.skinPic6);
+            this.panel1.Controls.Add(this.skinPic5);
+            this.panel1.Controls.Add(this.skinPic4);
+            this.panel1.Controls.Add(this.skinPic3);
+            this.panel1.Controls.Add(this.skinPic2);
+            this.panel1.Controls.Add(this.skinPic1);
+            this.panel1.Location = new System.Drawing.Point(1, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 612);
+            this.panel1.TabIndex = 0;
+            // 
+            // skinPic5
+            // 
+            this.skinPic5.Location = new System.Drawing.Point(315, 395);
+            this.skinPic5.Name = "skinPic5";
+            this.skinPic5.Size = new System.Drawing.Size(200, 200);
+            this.skinPic5.TabIndex = 4;
+            this.skinPic5.TabStop = false;
+            // 
+            // skinPic6
+            // 
+            this.skinPic6.Location = new System.Drawing.Point(571, 395);
+            this.skinPic6.Name = "skinPic6";
+            this.skinPic6.Size = new System.Drawing.Size(200, 200);
+            this.skinPic6.TabIndex = 5;
+            this.skinPic6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(307, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 48);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Skin Shop";
+            // 
+            // skinPic1
+            // 
+            this.skinPic1.Location = new System.Drawing.Point(71, 133);
+            this.skinPic1.Name = "skinPic1";
+            this.skinPic1.Size = new System.Drawing.Size(200, 200);
+            this.skinPic1.TabIndex = 0;
+            this.skinPic1.TabStop = false;
+            // 
+            // skinPic2
+            // 
+            this.skinPic2.Location = new System.Drawing.Point(315, 133);
+            this.skinPic2.Name = "skinPic2";
+            this.skinPic2.Size = new System.Drawing.Size(200, 200);
+            this.skinPic2.TabIndex = 1;
+            this.skinPic2.TabStop = false;
+            // 
+            // skinPic3
+            // 
+            this.skinPic3.Location = new System.Drawing.Point(571, 133);
+            this.skinPic3.Name = "skinPic3";
+            this.skinPic3.Size = new System.Drawing.Size(200, 200);
+            this.skinPic3.TabIndex = 2;
+            this.skinPic3.TabStop = false;
+            // 
+            // skinPic4
+            // 
+            this.skinPic4.Location = new System.Drawing.Point(71, 395);
+            this.skinPic4.Name = "skinPic4";
+            this.skinPic4.Size = new System.Drawing.Size(200, 200);
+            this.skinPic4.TabIndex = 3;
+            this.skinPic4.TabStop = false;
+            // 
+            // lb_name2
+            // 
+            this.lb_name2.AutoSize = true;
+            this.lb_name2.BackColor = System.Drawing.Color.Transparent;
+            this.lb_name2.Font = new System.Drawing.Font("Lucida Calligraphy", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_name2.ForeColor = System.Drawing.Color.Orange;
+            this.lb_name2.Location = new System.Drawing.Point(775, 94);
+            this.lb_name2.Name = "lb_name2";
+            this.lb_name2.Size = new System.Drawing.Size(125, 24);
+            this.lb_name2.TabIndex = 14;
+            this.lb_name2.Text = "name here";
+            // 
+            // lb_pts2
+            // 
+            this.lb_pts2.AutoSize = true;
+            this.lb_pts2.BackColor = System.Drawing.Color.Transparent;
+            this.lb_pts2.Font = new System.Drawing.Font("Lucida Calligraphy", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_pts2.ForeColor = System.Drawing.Color.Orange;
+            this.lb_pts2.Location = new System.Drawing.Point(800, 156);
+            this.lb_pts2.Name = "lb_pts2";
+            this.lb_pts2.Size = new System.Drawing.Size(99, 24);
+            this.lb_pts2.TabIndex = 13;
+            this.lb_pts2.Text = "pts here";
+            // 
+            // lb_pts
+            // 
+            this.lb_pts.AutoSize = true;
+            this.lb_pts.BackColor = System.Drawing.Color.Transparent;
+            this.lb_pts.Font = new System.Drawing.Font("Lucida Calligraphy", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_pts.ForeColor = System.Drawing.Color.Orange;
+            this.lb_pts.Location = new System.Drawing.Point(815, 132);
+            this.lb_pts.Name = "lb_pts";
+            this.lb_pts.Size = new System.Drawing.Size(44, 24);
+            this.lb_pts.TabIndex = 12;
+            this.lb_pts.Text = "pts";
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.BackColor = System.Drawing.Color.Transparent;
+            this.lb_name.Font = new System.Drawing.Font("Lucida Calligraphy", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_name.ForeColor = System.Drawing.Color.Orange;
+            this.lb_name.Location = new System.Drawing.Point(800, 70);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(76, 24);
+            this.lb_name.TabIndex = 11;
+            this.lb_name.Text = "Name";
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 635);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form5";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.skinPic4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox skinPic6;
+        private System.Windows.Forms.PictureBox skinPic5;
+        private System.Windows.Forms.PictureBox skinPic4;
+        private System.Windows.Forms.PictureBox skinPic3;
+        private System.Windows.Forms.PictureBox skinPic2;
+        private System.Windows.Forms.PictureBox skinPic1;
+        private System.Windows.Forms.Label lb_name2;
+        private System.Windows.Forms.Label lb_pts2;
+        private System.Windows.Forms.Label lb_pts;
+        private System.Windows.Forms.Label lb_name;
+    }
+}
---
+++ DBtest/Form5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBtest
+{
+    public partial class Form5 : MetroFramework.Forms.MetroForm
+    {
+        public Form5()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            // 스킨 이미지
+            //skin1.ImageLocation = ;
+            //skin2.ImageLocation = ;
+            //skin3.ImageLocation = ;
+            //skin4.ImageLocation = ;
+            //skin5.ImageLocation = ;
+            //skin6.ImageLocation = ;
+        }
+    }
+}
