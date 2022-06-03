16605782f85187749e7172cc9ca1f11ff4b45a6a
pj991207 <pj991207@gmail.com>
2022-06-03 15:06:08 +0900

    22.06.30
    22.06.30

---
+++ DBtest/Form3.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace DBtest
+{
+    partial class form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.user_picture = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_phone = new System.Windows.Forms.TextBox();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_ratio = new System.Windows.Forms.TextBox();
+            this.txt_lose = new System.Windows.Forms.TextBox();
+            this.txt_win = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.user_picture)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // user_picture
+            // 
+            this.user_picture.Enabled = false;
+            this.user_picture.InitialImage = null;
+            this.user_picture.Location = new System.Drawing.Point(99, 41);
+            this.user_picture.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.user_picture.Name = "user_picture";
+            this.user_picture.Size = new System.Drawing.Size(229, 250);
+            this.user_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.user_picture.TabIndex = 0;
+            this.user_picture.TabStop = false;
+            this.user_picture.Click += new System.EventHandler(this.user_picture_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(387, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "이름";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(387, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(387, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(387, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Phone";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Enabled = false;
+            this.txt_name.Location = new System.Drawing.Point(466, 38);
+            this.txt_name.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.ReadOnly = true;
+            this.txt_name.Size = new System.Drawing.Size(251, 25);
+            this.txt_name.TabIndex = 5;
+            // 
+            // txt_id
+            // 
+            this.txt_id.Enabled = false;
+            this.txt_id.Location = new System.Drawing.Point(466, 110);
+            this.txt_id.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.ReadOnly = true;
+            this.txt_id.Size = new System.Drawing.Size(251, 25);
+            this.txt_id.TabIndex = 6;
+            // 
+            // txt_email
+            // 
+            this.txt_email.Enabled = false;
+            this.txt_email.Location = new System.Drawing.Point(466, 179);
+            this.txt_email.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.ReadOnly = true;
+            this.txt_email.Size = new System.Drawing.Size(251, 25);
+            this.txt_email.TabIndex = 7;
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.Enabled = false;
+            this.txt_phone.Location = new System.Drawing.Point(466, 256);
+            this.txt_phone.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.ReadOnly = true;
+            this.txt_phone.Size = new System.Drawing.Size(251, 25);
+            this.txt_phone.TabIndex = 8;
+            this.txt_phone.TextChanged += new System.EventHandler(this.txt_phone_TextChanged);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Enabled = false;
+            this.btn_delete.Location = new System.Drawing.Point(770, 481);
+            this.btn_delete.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(86, 29);
+            this.btn_delete.TabIndex = 9;
+            this.btn_delete.Text = "삭제";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(643, 481);
+            this.btn_edit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(86, 29);
+            this.btn_edit.TabIndex = 10;
+            this.btn_edit.Text = "편집";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
+            // 
+            // btn_back
+            // 
+            this.btn_back.Location = new System.Drawing.Point(42, 481);
+            this.btn_back.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(86, 29);
+            this.btn_back.TabIndex = 11;
+            this.btn_back.Text = "뒤로";
+            this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_ratio);
+            this.groupBox1.Controls.Add(this.txt_lose);
+            this.groupBox1.Controls.Add(this.txt_win);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(99, 330);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(617, 119);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "전적";
+            // 
+            // txt_ratio
+            // 
+            this.txt_ratio.Enabled = false;
+            this.txt_ratio.Location = new System.Drawing.Point(353, 59);
+            this.txt_ratio.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_ratio.Name = "txt_ratio";
+            this.txt_ratio.ReadOnly = true;
+            this.txt_ratio.Size = new System.Drawing.Size(44, 25);
+            this.txt_ratio.TabIndex = 11;
+            // 
+            // txt_lose
+            // 
+            this.txt_lose.Enabled = false;
+            this.txt_lose.Location = new System.Drawing.Point(183, 59);
+            this.txt_lose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_lose.Name = "txt_lose";
+            this.txt_lose.ReadOnly = true;
+            this.txt_lose.Size = new System.Drawing.Size(44, 25);
+            this.txt_lose.TabIndex = 10;
+            // 
+            // txt_win
+            // 
+            this.txt_win.Enabled = false;
+            this.txt_win.Location = new System.Drawing.Point(25, 59);
+            this.txt_win.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_win.Name = "txt_win";
+            this.txt_win.ReadOnly = true;
+            this.txt_win.Size = new System.Drawing.Size(44, 25);
+            this.txt_win.TabIndex = 9;
+            this.txt_win.TextChanged += new System.EventHandler(this.txt_win_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(365, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 15);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "승률";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(198, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 15);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "패";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "승";
+            // 
+            // form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 562);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.txt_phone);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.user_picture);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "form3";
+            this.Text = "Form3";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.form3_FormClosed);
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.user_picture)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox user_picture;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_phone;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_ratio;
+        private System.Windows.Forms.TextBox txt_lose;
+        private System.Windows.Forms.TextBox txt_win;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ DBtest/Form4.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DBtest
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_profile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(711, 438);
+            this.btn_start.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(86, 29);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "입장하기";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_profile
+            // 
+            this.btn_profile.Location = new System.Drawing.Point(711, 32);
+            this.btn_profile.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_profile.Name = "btn_profile";
+            this.btn_profile.Size = new System.Drawing.Size(86, 50);
+            this.btn_profile.TabIndex = 1;
+            this.btn_profile.Text = "계정";
+            this.btn_profile.UseVisualStyleBackColor = true;
+            this.btn_profile.Click += new System.EventHandler(this.btn_profile_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(711, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "방만들기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 562);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_profile);
+            this.Controls.Add(this.btn_start);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form4_FormClosed);
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_profile;
+        private System.Windows.Forms.Button button1;
+    }
+}
